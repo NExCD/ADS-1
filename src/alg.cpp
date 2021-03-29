3db8b2168cfcb6557d2f7f0b5c2fe40685ec5923
NExCD <76452926+NExCD@users.noreply.github.com>
2021-03-29 19:42:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,14 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
- int left, right, mid, kol;
- kol = 0;
- left = 0;
- right = size - 1;
- while (left < right) {
-  mid = (left + right) / 2;
-  if (*(arr + mid) < value)
-   left = mid + 1;
+int left, right, mid, kol;
+kol = 0;
+left = 0;
+right = size - 1;
+while (left < right) {
+mid = (left + right) / 2;
+if (*(arr + mid) < value)
+left = mid + 1;
 		else
 			right = mid;
 	}
