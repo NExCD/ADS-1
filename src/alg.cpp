fe94f0f970be8c567aa9e88b514c55e48dc75045
NExCD <76452926+NExCD@users.noreply.github.com>
2021-03-29 19:40:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,13 @@
 
 int cbinsearch(int *arr, int size, int value) {
  int left, right, mid, kol;
-	kol = 0;
-	left = 0;
-	right = size - 1;
-	while (left < right) {
-		mid = (left + right) / 2;
-		if (*(arr + mid) < value)
-			left = mid + 1;
+ kol = 0;
+ left = 0;
+ right = size - 1;
+ while (left < right) {
+  mid = (left + right) / 2;
+  if (*(arr + mid) < value)
+   left = mid + 1;
 		else
 			right = mid;
 	}
